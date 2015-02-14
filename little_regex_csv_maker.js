516a3bb9e3a14718eb547442738b27b206fa39b7
kingofallcosmos <sgontare@mit.edu>
2015-02-14 03:05:29 -0500

    little csv/text function

--- little_regex_csv_maker.js
+++ little_regex_csv_maker.js
@@ -1,16 +1,20 @@
-var fs = require('fs');
-
-//function makeCSV(inputtextfile)
-var array = fs.readFileSync('sample.txt').toString().split("\n");
-fs.writeFile('sampleout.txt', 'ID,FY12,FY13,FY14\r\n')
-for(i in array) {
-	last = array[i].length-1;
-	if (!isNaN(array[i][0]) && !isNaN(array[i][last])){ 	// only writes out lines that begin and end with numbers
-		parsedline = array[i].replace(/[^\d\s]/g, '');	//remove all non-numerical characters and double spaces from line
-		condnsline = parsedline.replace(/\s+/g, ' ');
-		csvline = condnsline.replace(/\s/g, ',') + '\r\n';
-		console.log(csvline);
-    	fs.appendFile('sampleout.txt', csvline);
+function makeCSV(inputtextfile, outputfile) {
+	var fs = require('fs');
+	var array = fs.readFileSync(inputtextfile).toString().split("\n");
+	fs.writeFile(outputfile + '.txt', 'ID,FY12,FY13,FY14\r\n')
+	for(i in array) {
+		last = array[i].length-1;
+		if (!isNaN(array[i][0]) && !isNaN(array[i][last])){ 	// only writes out lines that begin and end with numbers
+			parsedline = array[i].replace(/[^\d\s]/g, '');	//remove all non-numerical characters and double spaces from line
+			condnsline = parsedline.replace(/\s+/g, ' ');	// cut out excess space
+			csvline = condnsline.replace(/\s/g, ',') + '\r\n';
+			console.log(csvline);
+    		fs.appendFile(outputfile + '.txt', csvline);
+    	}
     }
+csv()
+.from(fs.createReadStream('./test.txt'))
+.to(fs.createWriteStream('./file.csv'))
+return;
 }
 
