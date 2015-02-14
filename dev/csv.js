1ffb26b37d53eeecf90c1fb23e6c0f04854ba2f2
hathix <neelmehta@college.harvard.edu>
2015-02-14 14:35:29 -0500

    Removed unnecessary temporary files

--- dev/csv.js
+++
@@ -1,9 +0,0 @@
-// replace all stuff that looks like 1's with bona fide 1's
-var save1 = /([li]+) ?(\d)/ig;
-
-var str = "22I I00 Travel Out ol&apos;(&apos;ountry 0 3.474.800 7.l49,600";
-while(str.match(save1)){
-	str = str.replace(save1, "1$2");
-}
-
-console.log(str);
